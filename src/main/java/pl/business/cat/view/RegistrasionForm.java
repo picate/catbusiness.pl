cc921b4b3d765021d48f5ceb082f265001cdbc83
Daniel Dobrzyński <35843305+DanielDob@users.noreply.github.com>
2023-12-13 13:34:29 +0100

    regis form UI improved

--- src/main/java/pl/business/cat/view/RegistrasionForm.java
+++ src/main/java/pl/business/cat/view/RegistrasionForm.java
@@ -1,9 +1,13 @@
 package pl.business.cat.view;
 
+import java.util.ArrayList;
+
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.datetimepicker.DateTimePicker;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.NativeLabel;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 
@@ -15,40 +19,69 @@ import lombok.Setter;
 public class RegistrasionForm  extends FormLayout{
 
 	private TextField firstName, lastName, userName, email;
-	private DateTimePicker birth;
+	private DatePicker birth;
 	private PasswordField password, conformPassword;
 	private Button regisButton;
+	private ArrayList<HorizontalLayout> horizontalL;
+	private NativeLabel registration;
 	private H1 pi;
 	
 	public RegistrasionForm() {
 		this.getClassNames().add("regis-form");
 		this.getElement().getStyle().set("max-width", "100vw");
-		firstName = new TextField();
-		firstName.setLabel("Imię");
-		lastName= new TextField();
-		lastName.setLabel("Nazwisko");
-		email = new TextField();
-		email.setLabel("Emial");
-		userName= new TextField();
-		userName.setLabel("Nazwa gracza");
-		birth = new DateTimePicker();
-		birth.setLabel("Data urodzin");
-		password = new PasswordField();
-		password.setLabel("Hasło");
-		conformPassword = new PasswordField();
-		conformPassword.setLabel("Powtórz hasło");
-		regisButton = new Button("Sing up");
-		pi = new H1("Picate");
-		pi.setId("picate");
-		add(pi,firstName,lastName,userName,email,password,conformPassword,regisButton);
+		childSet();
 		 // Max width of the Form
-       setMaxWidth("500px");
+       setMaxWidth("300px");
        setColspan(pi, 2);
        // Allow the form layout to be responsive.
        // On device widths 0-490px we have one column.
        // Otherwise, we have two columns.
        setResponsiveSteps(
                new ResponsiveStep("0", 1, ResponsiveStep.LabelsPosition.TOP),
-               new ResponsiveStep("490px", 2, ResponsiveStep.LabelsPosition.TOP));
+               new ResponsiveStep("750px", 2, ResponsiveStep.LabelsPosition.TOP));
+	}
+
+	private void childSet() {
+		horizontalL = new ArrayList<>();
+		registration = new NativeLabel("Registrasion");
+		registration.setId("regis-label");
+		HorizontalLayout regisHL = new HorizontalLayout(registration);
+		regisHL.setId("regis-hl-label");
+		firstName = new TextField();
+		firstName.setLabel("First name");
+		firstName.setClassName("data-regis");
+		lastName= new TextField();
+		lastName.setLabel("Last name");
+		lastName.setClassName("data-regis");
+		horizontalL.add(new HorizontalLayout(firstName,lastName));
+		email = new TextField();
+		email.setLabel("Email");
+		email.setClassName("data-regis");
+		birth = new DatePicker();
+		birth.setLabel("Date of birth");
+		birth.setClassName("data-regis");
+		horizontalL.add(new HorizontalLayout(email,birth));
+		userName= new TextField();
+		userName.setLabel("User name");
+		userName.setClassName("data-regis");
+		horizontalL.add(new HorizontalLayout(userName));
+		password = new PasswordField();
+		password.setLabel("Password");
+		password.setClassName("data-regis");
+		conformPassword = new PasswordField();
+		conformPassword.setLabel("Repeat password");
+		conformPassword.setClassName("data-regis");
+		horizontalL.add(new HorizontalLayout(password,conformPassword));
+		regisButton = new Button("Sing up");
+		regisButton.setId("regis-button");
+		regisButton.setClassName("sign");
+		horizontalL.add(new HorizontalLayout(regisButton));
+		pi = new H1("Picate");
+		pi.setId("picate");
+		horizontalL.forEach(e->{setColspan(e, 2);
+				e.setClassName("layout-data-regis");
+			});
+		add(pi,regisHL,horizontalL.get(0),horizontalL.get(1),horizontalL.get(2),horizontalL.get(3),horizontalL.get(4));
+		
 	}
 }
